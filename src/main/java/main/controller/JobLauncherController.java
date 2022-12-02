f0a1b7350af6c7f98c35e40d9eecc5acf67dc5b8
edwin <ronyargueta123@gmail.com>
2022-12-02 16:45:52 -0600

    refactoring code

--- src/main/java/main/controller/JobLauncherController.java
+++ src/main/java/main/controller/JobLauncherController.java
@@ -1,7 +1,16 @@
 package main.controller;
 
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +22,16 @@ import java.io.IOException;
 @RestController
 public class JobLauncherController {
     @Autowired
-    private DefaultLinker linker;
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job processJob;
 
     @GetMapping("/start")
-    protected String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       // linker = new DefaultLinker();
-
-       return (linker.executeJob());
+    protected String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+        jobLauncher.run(processJob, jobParameters);
+        return "Batch job has been invoked";
     }
 }
