14ea732f0900c89c3a477e5a6484b30914422d0b
edwin <ronyargueta123@gmail.com>
2022-12-13 07:05:24 -0600

    refactoring code

--- src/main/java/main/controller/JobLauncherController.java
+++ src/main/java/main/controller/JobLauncherController.java
@@ -10,7 +10,6 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
